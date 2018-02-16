a4869a4c0218ca29f6cba15fab976aa11167bb82
zealz-matsumoto <h_matsumoto@zealz.co.jp>
2018-02-16 15:36:08 +0900

    testAPI作成

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/',function() {
+    return 'response';
+});
+Route::get('/create/{value}',function($value){
+    return 'created';
+});
